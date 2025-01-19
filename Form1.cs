f9298300429e775650d61474a1cfe2592ad59fd7
Muhammet Emin Karlıtepe <123209132+EminKarlitepe@users.noreply.github.com>
2025-01-19 14:31:40 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEğitimKampı301.EFProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Guide.ToList();
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.Name = txtName.Text;
+            guide.Surname = txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges();
+            MessageBox.Show("Rehber Başarı ile Eklendi!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var removeValue = db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber Başarı ile Silindi!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.Name = txtName.Text;
+            updateValue.Surname = txtSurname.Text;
+            db.SaveChanges();
+            MessageBox.Show("Rehber Başarı ile Güncellendi!","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var values = db.Guide.Where(x=>x.GuideId==id).ToList();
+            dataGridView1.DataSource= values;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmStatistics form = new FrmStatistics();
+            form.Show();
+            this.Hide();
+
+        }
+    }
+}
---
+++ FrmStatistics.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEğitimKampı301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+
+            lblLocationCount.Text = db.Location.Count().ToString();
+            lblSumCapacity.Text = db.Location.Sum(x => x.Capacity).ToString();
+            lblGuide.Text = db.Guide.Count().ToString();
+            lblAvgCapacity.Text = db.Location.Average(x => x.Capacity).ToString();
+            lblAvgLocationPrice.Text = db.Location.Average(x => x.Price).ToString();
+
+            int lastCountryId = db.Location.Max(x=>x.LocatonId);
+            lblLastCountryName.Text = db.Location.Where(x=>x.LocatonId==lastCountryId).Select(y=>y.Country).
+                FirstOrDefault();
+
+            lblKapadokyaCapacity.Text = db.Location.Where(x => x.City == "Kapadokya")
+                .Select(y => y.Capacity).FirstOrDefault().ToString();
+
+            lblTurkiyeCapacityAvg.Text = db.Location.Where(x => x.Country == "Türkiye").
+                Average(y => y.Capacity).ToString();
+
+            var romeGuideId = db.Location.Where(x=>x.City=="Roma").Select(y=>y.GuideId).FirstOrDefault();
+            lblRomeGuideName.Text = db.Guide.Where(x=>x.GuideId == romeGuideId).Select(y=>y.Name + " " + y.Surname).
+                FirstOrDefault().ToString();
+
+            var maxCapacity = db.Location.Max(x=>x.Capacity);
+            lblMaxCapacityCountry.Text = db.Location.Where(x=>x.Capacity==maxCapacity).
+                Select(y=>y.City).FirstOrDefault().ToString();
+
+            var maxPrice = db.Location.Max(x=>x.Price);
+            lblMaxPriceLocation.Text = db.Location.Where(x => x.Price == maxPrice).
+                Select(y => y.City).FirstOrDefault().ToString();
+
+            var guideIdByAysegulCinar = db.Guide.Where(x=>x.Name =="Ayşegül" && x.Surname=="Çınar").
+                Select(y=>y.GuideId).FirstOrDefault();
+
+            lblAysegulCinarLocationCount.Text = db.Location.Where(x=>x.GuideId == guideIdByAysegulCinar).
+                Count().ToString();
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.Show();
+            this.Close();
+        }
+    }
+}
